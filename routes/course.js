47472327f4770e7a86a5125f12f82269d40a47e8
justarya <just1arya@gmail.com>
2019-07-31 16:34:06 +0700

    View 3

--- routes/course.js
+++ routes/course.js
@@ -3,6 +3,6 @@ const CourseController = require('../controller/CourseController');
 
 routes.get('/', (res,req) => req.render('./course'));
 routes.get('/:id', (res,req) => req.render('./course/item'));
-routes.get('/:id/video/:id');
+routes.get('/:id/video/:id', (res,req) => req.render('./course/video'));
 
 module.exports = routes;
