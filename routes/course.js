40a8936f0ba7e25c3b77b62eb3f9f9fff6b85919
justarya <just1arya@gmail.com>
2019-07-31 15:42:59 +0700

    Views

--- routes/course.js
+++ routes/course.js
@@ -1,8 +1,8 @@
 const routes = require('express').Router();
-const CourseController = require('../controllers/CourseController');
+const CourseController = require('../controller/CourseController');
 
-routes.get('/', (res,req) => res.send('test'));
-routes.get('/:id');
+routes.get('/', (res,req) => req.render('./course'));
+routes.get('/:id', (res,req) => req.render('./course/item'));
 routes.get('/:id/video/:id');
 
 module.exports = routes;
