9c12df04d9dfb50f743b1461c498a9707a4e462d
justarya <just1arya@gmail.com>
2019-08-02 10:51:35 +0700

    Fix voucher

--- controller/VoucherController.js
+++ controller/VoucherController.js
@@ -3,25 +3,27 @@ const User = require('../models').User;
 const Model = require('../models');
 class VoucherController {
   static validateVoucher(req,res){
-    Voucher.update({
-      status: false
-    },{
-      where: {
-        status: true,
-        code: req.body.code.toUpperCase()
-      }
-    })
+    let dataVoucher;
+    Voucher.findOne({where: {code:req.body.code.toUpperCase()}})
     .then((data) => {
+      dataVoucher = data;
       if(!data){
         throw new Error('Voucher code is not valid')
       }else{
-        return Voucher.findOne({where: {code:req.body.code.toUpperCase()}})
+        return Voucher.update({
+          status: false
+        },{
+          where: {
+            status: true,
+            code: req.body.code.toUpperCase()
+          }
+        })
       }
     })
     .then((data) => {
-      console.log(data.voucherValue);
+      console.log(dataVoucher.voucherValue);
       return User.update({
-        balance: Model.sequelize.literal('balance +'+data.voucherValue)
+        balance: Model.sequelize.literal('balance +'+dataVoucher.voucherValue)
       },{
         where: {
           id: req.session.idUser
